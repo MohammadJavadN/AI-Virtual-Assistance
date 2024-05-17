0d77c07faa74060805b466d5f459b7498d04eef7
MohammadJavadN <norozim17@gmail.com>
2024-05-17 23:22:21 +0330

    Updated HomeFragment to include speech recognition and text-to-speech functionality, and integrated OpenAI API for chat responses.

--- app/src/main/java/com/example/ai_virtual_assistance/MainActivity.java
+++ app/src/main/java/com/example/ai_virtual_assistance/MainActivity.java
@@ -2,63 +2,28 @@ package com.example.ai_virtual_assistance;
 
 import static android.Manifest.permission.RECORD_AUDIO;
 
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
-import android.speech.tts.TextToSpeech;
-import android.view.View;
 import android.view.Menu;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.example.ai_virtual_assistance.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-    private TextView textView;
-    private EditText editText;
-
-    private String stringURLEndPoint = "https://api.openai.com/v1/chat/completions";
-    private String stringAPIKey = "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";
-    private String stringOutput = "";
-
-    private TextToSpeech textToSpeech;
-    private SpeechRecognizer speechRecognizer;
-    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,138 +53,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-        textView = binding.navView.getHeaderView(0).findViewById(R.id.gptTextView);
-        editText = binding.navView.getHeaderView(0).findViewById(R.id.editText);
-
-        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int i) {
-                textToSpeech.setSpeechRate((float) 0.8);
-            }
-        });
-
-        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-        speechRecognizer.setRecognitionListener(new RecognitionListener() {
-            @Override
-            public void onReadyForSpeech(Bundle bundle) {
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-            }
-
-            @Override
-            public void onRmsChanged(float v) {
-            }
-
-            @Override
-            public void onBufferReceived(byte[] bytes) {
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-            }
-
-            @Override
-            public void onError(int i) {
-            }
-
-            @Override
-            public void onResults(Bundle bundle) {
-                ArrayList<String> matches = bundle.getStringArrayList(speechRecognizer.RESULTS_RECOGNITION);
-                if (matches != null) {
-                    String string = matches.get(0);
-                    editText.setText(string);
-                    chatGPTModel(string);
-                }
-            }
-
-            @Override
-            public void onPartialResults(Bundle bundle) {
-            }
-
-            @Override
-            public void onEvent(int i, Bundle bundle) {
-            }
-        });
-    }
-
-    public void buttonAssist(View view) {
-        if (textToSpeech.isSpeaking()) {
-            textToSpeech.stop();
-            return;
-        }
-        stringOutput = "";
-        speechRecognizer.startListening(intent);
-    }
-
-    private void chatGPTModel(String stringInput) {
-        textView.setText("In Progress ...");
-        textToSpeech.speak("In Progress", TextToSpeech.QUEUE_FLUSH, null, null);
-
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("model", "gpt-3.5-turbo");
-
-            JSONArray jsonArrayMessage = new JSONArray();
-            JSONObject jsonObjectMessage = new JSONObject();
-            jsonObjectMessage.put("role", "user");
-            jsonObjectMessage.put("content", stringInput);
-            jsonArrayMessage.put(jsonObjectMessage);
-
-            jsonObject.put("messages", jsonArrayMessage);
-
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
-                stringURLEndPoint, jsonObject, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                String stringText = null;
-                try {
-                    stringText = response.getJSONArray("choices")
-                            .getJSONObject(0)
-                            .getJSONObject("message")
-                            .getString("content");
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
-                stringOutput = stringOutput + stringText;
-                textView.setText(stringOutput);
-                textToSpeech.speak(stringOutput, TextToSpeech.QUEUE_FLUSH, null, null);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> mapHeader = new HashMap<>();
-                mapHeader.put("Authorization", "Bearer " + stringAPIKey);
-                mapHeader.put("Content-Type", "application/json");
-
-                return mapHeader;
-            }
-
-            @Override
-            protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
-                return super.parseNetworkResponse(response);
-            }
-        };
-
-        int intTimeoutPeriod = 60000; // 60 seconds timeout duration defined
-        RetryPolicy retryPolicy = new DefaultRetryPolicy(intTimeoutPeriod,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-        jsonObjectRequest.setRetryPolicy(retryPolicy);
-        Volley.newRequestQueue(getApplicationContext()).add(jsonObjectRequest);
     }
 
     @Override
